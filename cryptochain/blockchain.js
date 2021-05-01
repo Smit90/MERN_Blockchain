1d8accfdfe525a268e11430612239358c687f449
Smit Patel <smitp90333@gmail.com>
2021-05-01 18:50:25 +0530

    api, network and chain sync

--- cryptochain/blockchain.js
+++ cryptochain/blockchain.js
@@ -44,7 +44,7 @@ class Blockchain {
 
             if (hash !== validatedHash) return false;
 
-            if ((lastDifficulty - difficulty) > 1) return false;
+            if (Math.abs(lastDifficulty - difficulty) > 1) return false;
         }
 
         return true;
