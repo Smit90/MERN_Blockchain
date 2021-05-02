6f13b1e228d4a0f039b5b1ce924e7ba551f97fd7
Smit Patel <smitp90333@gmail.com>
2021-05-02 17:39:33 +0530

    code organization

--- cryptochain/index.js
+++ cryptochain/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const Blockchain = require('./blockchain')
-const PubSub = require('./pubsub')
+const Blockchain = require('./blockchain/index')
+const PubSub = require('./app/pubsub')
 
 const app = express()
 const blockchain = new Blockchain()
